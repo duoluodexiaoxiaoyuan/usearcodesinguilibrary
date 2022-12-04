8bddd5064af98f6deff3ea2ad4797c9d3e918e4b
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-12-04 16:08:42 +0800

    feature:熟悉了tab的使用

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Tabs, Radio, Typography } from '@arco-design/web-react';
+const TabPane = Tabs.TabPane;
+const style = {
+  textAlign: 'center',
+  marginTop: 20,
+};
 
 function App() {
+  const [type, setType] = useState('line');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Radio.Group name='type' value={type} onChange={setType} style={{ marginBottom: 40 }}>
+        <Radio value='line'>line</Radio>
+        <Radio value='card'>card</Radio>
+        <Radio value='card-gutter'>card-gutter</Radio>
+        <Radio value='text'>text</Radio>
+        <Radio value='rounded'>rounded</Radio>
+        <Radio value='capsule'>capsule</Radio>
+      </Radio.Group>
+
+      <Tabs type={type}>
+        <TabPane key='1' title='Tab 1'>
+          <Typography.Paragraph style={style}>Content of Tab Panel 1</Typography.Paragraph>
+        </TabPane>
+        <TabPane key='2' title='Tab 2' disabled>
+          <Typography.Paragraph style={style}>Content of Tab Panel 2</Typography.Paragraph>
+        </TabPane>
+        <TabPane key='3' title='Tab 3'>
+          <Typography.Paragraph style={style}>Content of Tab Panel 3</Typography.Paragraph>
+        </TabPane>
+        <TabPane key='4' title='Tab 4'>
+          <Typography.Paragraph style={style}>Content of Tab Panel 4</Typography.Paragraph>
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
